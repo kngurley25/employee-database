c0da33eb2736383d4d32464a044f5f31ec148e67
kngurley <kngurley25@gmail.com>
2021-12-02 17:40:00 -0600

    add npm packages

--- db/seeds.sql
+++ db/seeds.sql
@@ -6,11 +6,11 @@ VALUES
 
 INSERT INTO roles (title, salary, department_id)
 VALUES
-    ("Manager", 150,00, 1),
-    ("Architect", 100,000, 2),
-    ("Electrical Engineer", 120,000, 3),
-    ("Mechanical Engineer", 120,000, 3),
-    ("Process Engineer", 130,000, 3);
+    ("Manager", 150000, 1),
+    ("Architect", 100000, 2),
+    ("Electrical Engineer", 120000, 3),
+    ("Mechanical Engineer", 120000, 3),
+    ("Process Engineer", 130000, 3);
 
 INSERT INTO employees (first_name, last_name, role_id, manager_id)
 VALUES
