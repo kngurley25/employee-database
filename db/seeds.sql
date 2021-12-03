0d116c9111fe0b3a0701cddcb21d6bad4aa8163d
kngurley <kngurley25@gmail.com>
2021-12-02 21:21:09 -0600

    update seeds file

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,21 +1,30 @@
 INSERT INTO departments (name)
 VALUES
-    ("Management"),
     ("Architecture"),
-    ("Engineering");
+    ("Engineering"),
+    ("Construction");
 
 INSERT INTO roles (title, salary, department_id)
 VALUES
-    ("Manager", 150000, 1),
-    ("Architect", 100000, 2),
-    ("Electrical Engineer", 120000, 3),
-    ("Mechanical Engineer", 120000, 3),
-    ("Process Engineer", 130000, 3);
+    ("Design Project Manager", 150000, 2),
+    ("Architect", 100000, 1),
+    ("Electrical Engineer", 120000, 2),
+    ("Mechanical Engineer", 120000, 2),
+    ("Process Engineer", 130000, 2),
+    ("Electrical Superintendent", 140000, 3),
+    ("Construction Project Manager", 180,000, 3);
 
 INSERT INTO employees (first_name, last_name, role_id, manager_id)
 VALUES
     ("Joey", "Fitzgerald", 1, NULL),
-    ("Zachary", "Denton", 2, NULL),
+    ("Jared", "Wirth", 1, NULL),
+    ("Zachary", "Denton", 2, 1),
+    ("Bob", "Matthews", 2, 1),
     ("Kristin", "Gurley", 3, 1),
-    ("John", "Gaito", 4, NULL),
-    ("Brenna", "Brazenski", 5, 1);
+    ("Mike", "Talbot", 3, 1),
+    ("John", "Gaito", 4, 2),
+    ("Kyle", "Chadwick", 4, 2),
+    ("Brenna", "Brazenski", 5, 1),
+    ("Kevin", "Wellington", 5, 2),
+    ("Rick", "Hatfield", 6, 7),
+    ("Leah", "Baker", 7, NULL);
