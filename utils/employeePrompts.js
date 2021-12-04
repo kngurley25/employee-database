ce73dea7f695cc48b35afee9a9172928cf503f54
kngurley <kngurley25@gmail.com>
2021-12-04 14:12:19 -0600

    updated employee prompt to include manager

--- utils/employeePrompts.js
+++ utils/employeePrompts.js
@@ -1,5 +1,6 @@
 const Query = require("../lib/Query");
 const roles = [];
+const managers = [];
 new Query().getRoleTitles()
     .then(([rows, fields]) => {
         const rolesArray = rows.map(obj => {
@@ -8,6 +9,16 @@ new Query().getRoleTitles()
         for (let i = 0; i < rolesArray.length; i ++) {
             const listItem = rolesArray[i];
             roles.push(listItem);
+        }
+    })
+new Query().getManagerFullNames()
+    .then(([rows, fields]) => { 
+        const managersArray = rows.map(obj => {
+            return obj.full_name;
+        })
+        for (let i = 0; i < managersArray.length; i ++) {
+            const listItem = managersArray[i];
+            managers.push(listItem);
         }
     })
 
@@ -43,6 +54,12 @@ const employeePrompts = [
         name: "role",
         message: "What is the employee's role?",
         choices: roles
+    },
+    {
+        type: "list",
+        name: "manager",
+        message: "Who is the employee's manager?",
+        choices: managers
     }
 ]
 
